b35354a93f343f4846de847b354b422cf6a448ce
sngoyo <salim@kali>
2023-03-17 01:19:50 +0300

    Changes in pause and resume section

--- src/components/leftTime.js
+++ src/components/leftTime.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import dayjs from "dayjs";
 
 export function FormatTime(time) {
@@ -19,5 +19,6 @@ function getRemainingSeconds(nowdayjs, userdayjs) {
 
 function getRemainingMinutes(nowdayjs, userdayjs) {
     let remainingMinutes = userdayjs.diff(nowdayjs, 'minutes') % 60;
+    console.log(remainingMinutes)
     return Math.abs(remainingMinutes);
 }
